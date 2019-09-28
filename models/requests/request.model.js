63f7c7eedd913331d82841ce3f205f50942fe4e9
Code7unner <cherbadzhimaxim@gmail.com>
2019-09-28 16:44:34 +1000

    Fix request schema

--- models/requests/request.model.js
+++ models/requests/request.model.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     address: { type: String, required: true },
-    status: { type: String, required: true },
-    request: { type: String, required: true },
+    status: { type: String },
+    request: { type: String },
     feedback: {
         rating: { type: Number, default: 0 },
         text: { type: String, default: "" }
